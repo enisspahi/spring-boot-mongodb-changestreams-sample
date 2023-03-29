8277850a1fd2c86a926657b0c831530fbe29d135
enisspahi <enis.spahi@openvalue.de>
2023-03-29 15:38:12 +0200

    rename main class

--- src/main/kotlin/de/openvalue/resilience/ResilienceSampleApplication.kt
+++ src/main/kotlin/de/openvalue/resilience/ResilienceSampleApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringBootMongodbChangestreamsSampleApplication
+class ResilienceSampleApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringBootMongodbChangestreamsSampleApplication>(*args)
+	runApplication<ResilienceSampleApplication>(*args)
 }
